86d129c4fbad589195900d5635515daefa7632d8
Kenny Lu <kenny@whynotkenny.com>
2018-12-23 13:40:47 -0500

    include form & http module / instantiate service in component / setup form handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AppService } from "./app.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'wedding';
+  title = "wedding";
+  formInputs = {};
+
+  constructor(private _appService: AppService) {}
+
+  handleSubmit(inputs) {
+    console.log("inputs: ", inputs);
+
+    // this._appService.submitToServer(inputs).subscribe(data => {
+    //   console.log("data: ", data);
+
+    //   this.formInputs = data;
+    // });
+  }
 }
