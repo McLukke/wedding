22804f209252d1cebe276d07f5d929b7426cd8d0
Kenny Lu <kenny@whynotkenny.com>
2018-12-24 10:56:18 -0500

    init db

---
+++ api/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "whynot14_mclukke";
+$dbPassword = "Password!23";
+$dbName = "whynot14_wedding";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
