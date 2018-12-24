6527f7c135f7662186b3c01b942305d51b2ab67e
Kenny Lu <kenny@whynotkenny.com>
2018-12-24 12:05:02 -0500

    check for server request method instead

--- api/invitation.php
+++ api/invitation.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // include_once 'dbh.php';
 
   // $guest = mysqli_real_escape_string($conn, $_POST['guest']);
@@ -8,7 +8,10 @@ if (isset($_POST['submit'])) {
   $data = (object) array('id' => '123');
   header('Content-Type: application/json');
   echo json_encode($data);
-} else { 
-  header("Location: ../index.html");
+} else {
+  // header("Location: ../index.html");
+
+  $errorData = (object) array('badId' => 'test');
+  echo json_encode($errorData);
   exit();
 }
