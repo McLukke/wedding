999f398f9d21f07198e0211fa5e2bbe76c07c897
Kenny Lu <kenny@whynotkenny.com>
2018-12-24 15:14:35 -0500

    server side null checks with db queries / successful insert to db

--- api/invitation.php
+++ api/invitation.php
@@ -1,17 +1,43 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // include_once 'dbh.php';
+  include_once 'dbh.php';
 
-  // $guest = mysqli_real_escape_string($conn, $_POST['guest']);
+  $request = json_decode(file_get_contents("php://input"));
 
-  $data = (object) array('id' => '123');
-  header('Content-Type: application/json');
-  echo json_encode($data);
+  $name = mysqli_real_escape_string($conn, $request->name);
+  $hasGuest = mysqli_real_escape_string($conn, $request->hasGuest);
+  $guestName = mysqli_real_escape_string($conn, $request->guestName);
+  $hasKids = mysqli_real_escape_string($conn, $request->hasKids);
+  $childName = mysqli_real_escape_string($conn, $request->childName);
+  $vegetarian = mysqli_real_escape_string($conn, $request->vegetarian);
+
+  // Error handlers
+  if (empty($name)
+    || ($hasGuest && empty($guestName))
+    || ($hasKids && empty($childName))
+  ) {
+    header("Status: 400 Bad Request");
+    exit();
+  } else {
+    // check existing
+    $sql = "SELECT * FROM users WHERE name='$name'";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+      header("Status: 409 User Exists");
+      exit();
+    } else {
+      // insert into db
+      $sql = "INSERT INTO users (name, has_guest, guest_name, has_kids, child_name, vegetarian) VALUES ('$name', '$hasGuest', '$guestName', '$hasKids', '$childName', '$vegetarian');";
+      mysqli_query($conn, $sql);
+
+      header('Status: 201 Created');
+      exit();
+    }
+  }
 } else {
-  // header("Location: ../index.html");
-
-  $errorData = (object) array('badId' => 'test');
-  echo json_encode($errorData);
+  header("Location: ../index.html");
   exit();
 }
