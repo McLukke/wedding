fdbcbe4221f68f4d1fe9f4e1aa8ade03ca32249d
Kenny Lu <kenny@whynotkenny.com>
2018-12-24 11:27:17 -0500

    rename form fields / default values / catch & log errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,27 @@ import { AppService } from "./app.service";
 })
 export class AppComponent {
   title = "wedding";
-  formInputs = {};
+  formInputs = {
+    name: "",
+    hasGuest: false,
+    guestName: "",
+    hasKids: false,
+    childName: "",
+    vegetarian: false
+  };
 
   constructor(private _appService: AppService) {}
 
   handleSubmit() {
     console.log("this.formInputs: ", this.formInputs);
 
-    this._appService.submitToServer(this.formInputs).subscribe(res => {
-      console.log("res: ", res);
-    });
+    this._appService.submitToServer(this.formInputs).subscribe(
+      res => {
+        console.log("res: ", res);
+      },
+      err => {
+        console.log("err: ", err);
+      }
+    );
   }
 }
