70c524b2f1675178705606181597203f1d8abc8e
Kenny Lu <kenny@whynotkenny.com>
2019-01-02 19:22:11 -0500

    install ant design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AppService } from "./app.service";
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [AppService],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgZorroAntdModule, BrowserAnimationsModule],
+  providers: [AppService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
