095e789e5eaf5bcff93bd17a6a41fff3317d7f79
Kenny Lu <kenny@whynotkenny.com>
2019-01-06 10:35:29 -0500

    styling form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AppService } from "./app.service";
 export class AppComponent {
   title = "wedding";
   formInputs = {
-    name: "",
+    yourName: "",
     hasGuest: false,
     guestName: "",
     hasKids: false,
