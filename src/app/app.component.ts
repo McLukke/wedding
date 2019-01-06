f5e31957e406e70b668752d1f4abafafa3daf898
Kenny Lu <kenny@whynotkenny.com>
2019-01-06 17:27:33 -0500

    setup localStorage and server errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,45 @@
 import { Component } from "@angular/core";
 import { AppService } from "./app.service";
 
+const initFormState = {
+  name: "",
+  hasGuest: false,
+  guestName: "",
+  hasKids: false,
+  childName: "",
+  vegetarian: false
+};
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  formInputs = {
-    name: "",
-    hasGuest: false,
-    guestName: "",
-    hasKids: false,
-    childName: "",
-    vegetarian: false
-  };
+  formInputs = { ...initFormState };
 
   tabs = ["Venue", "RSVP", "Registry"];
 
   selectedTab = 1;
 
+  serverError;
+
+  rsvpAlready = Boolean(window.localStorage.getItem("rsvpAlready") === "true");
+
   constructor(private _appService: AppService) {}
 
   handleSubmit() {
-    console.log("this.formInputs: ", this.formInputs);
+    this.serverError = undefined;
 
-    // this._appService.submitToServer(this.formInputs).subscribe(
-    //   res => {
-    //     console.log("res: ", res);
-    //   },
-    //   err => {
-    //     console.log("err: ", err);
-    //   }
-    // );
+    this._appService.submitToServer(this.formInputs).subscribe(
+      res => {
+        this.formInputs = { ...initFormState };
+        window.localStorage.setItem("rsvpAlready", "true");
+      },
+      err => {
+        this.serverError =
+          "Sorry, something unexpected has occurred on our servers.";
+      }
+    );
   }
 }
